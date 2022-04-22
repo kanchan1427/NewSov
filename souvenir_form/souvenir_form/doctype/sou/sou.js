bf5efd7f024d4c73db0a8048c56a4e3306960c66
Pawandeep <pawandeep1815343@gndec.ac.in>
2022-04-22 20:01:23 +0530

    Validation and Data Fetch

--- souvenir_form/souvenir_form/doctype/sou/sou.js
+++ souvenir_form/souvenir_form/doctype/sou/sou.js
@@ -2,7 +2,42 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('sou', {
-	// refresh: function(frm) {
+	
+		// refresh: function(frm) {
+			onload:function(frm){
+			frappe.call({
+				method: "souvenir_form.souvenir_form.doctype.__init__.get_full_user_name",
+				args: {"user": frappe.session.user },
+				callback: function(r) {
+					if(r.message) {
+						frm.set_value("name1", r.message)
+					}
+				}
+			})
+	
+			frappe.call({
+				method: "souvenir_form.souvenir_form.doctype.__init__.get_contact_no",
+				args: {"user": frappe.session.user },
+				callback: function(r) {
+					if(r.message) {
+						frm.set_value("contact_no", r.message)
+						
+					}
+				}
+			})
+			frappe.call({
+				method: "souvenir_form.souvenir_form.doctype.__init__.get_email",
+				args: {"user": frappe.session.user },
+				callback: function(r) {
+					if(r.message) {
+						frm.set_value("email", r.message)
+						
+					}
+				}
+			})
+		
+		}
+		// 
+		});
+			
 
-	// }
-});
--- souvenir_form/souvenir_form/web_form/souvenir/souvenir.js
+++ souvenir_form/souvenir_form/web_form/souvenir/souvenir.js
@@ -1,95 +1,74 @@
-frappe.ready(function() {
-  frappe.web_form.after_load=()=>{
-   frappe.msgprint('Please fill all values carefully');
+frappe.ready(function () {
+    frappe.msgprint('Please fill all values carefully');
 
 
-     frappe.web_form.on('contact_no',(field,value)=>{
-        if (value.toString().length > 10 || value.toString().length < 10 ) {
-         frappe.throw('Please Enter a Valid phone number');
-         field.set_value(0);
+    // Validations for the Fields
+    frappe.web_form.validate = () => {
 
-     }
-   
-     });
+        let data = frappe.web_form.get_values();
+        console.log(data)
+        var re = /^[A-Za-z]+$/;
 
-     frappe.web_form.on('university_roll_no',(field,value)=>{
-      if (value.toString().length > 7 || value.toString().length < 7 ) {
-       frappe.throw('Please Enter a Valid University Roll No.');
-       field.set_value(0);
+        if (data.university_roll_no.toString().length > 7 || data.university_roll_no.toString().length < 7) {
+            frappe.throw('Please Enter a Valid University Roll No.');
+            return false;
+        }
+        
 
-   }
- 
-   });
+        else if (data.contact_no.toString().length > 10 || data.contact_no.toString().length < 10 ||data.contact_no == " " ) {
+            frappe.throw('Please Enter a Valid phone number');
+            return false;
 
-   frappe.web_form.on('college_roll_no',(field,value)=>{
-      if (value.toString().length > 7 || value.toString().length < 7 ) {
-       frappe.throw('Please Enter a valid Roll NO.');
-       field.set_value(0);
+        }
+        
 
-   }
- 
-   });
+        else if (data.college_roll_no.toString().length > 7 || data.college_roll_no.toString().length < 7) {
+            frappe.throw('Please Enter a valid College Roll NO.');
+            return false;
 
-// Friends data validation
+        }
+      
 
-frappe.web_form.on('friend_1s_urn',(field,value)=>{
-   if (value.toString().length > 7 || value.toString().length < 7 ) {
-    frappe.throw('Please Enter a Valid University_Roll No.');
-    field.set_value(0);
+// Validates Friends deatils
 
-}
+     if(data.friend_1s_crn == null || data.friend_1s_urn == null || data.friend_2s_crn == null|| data.friend_2s_urn == null ){
+        frappe.throw('Please Enter Friends Roll Numbers');
+        if (data.friend_1s_crn.toString().length > 7 || data.friend_1s_crn.toString().length < 7) {
+            frappe.throw('Please Enter a valid College_Roll NO.');
+            return false;
 
-});
+        }
 
-frappe.web_form.on('friend_1s_crn',(field,value)=>{
-   if (value.toString().length > 7 || value.toString().length < 7 ) {
-    frappe.throw('Please Enter a valid College_Roll NO.');
-    field.set_value(0);
+        else if (data.friend_1s_urn.toString().length > 7 || data.friend_1s_urn.toString().length < 7) {
+            frappe.throw('Please Enter a Valid University_Roll No.');
+            return false;
+        }
 
-}
+        else if (data.friend_2s_urn.toString().length > 7 || data.friend_2s_urn.toString().length < 7) {
+            frappe.throw('Please Enter a valid University_Roll NO.');
+            return false;
 
-});
+        }
 
-frappe.web_form.on('friend_2s_urn',(field,value)=>{
-   if (value.toString().length > 7 || value.toString().length < 7 ) {
-    frappe.throw('Please Enter a Valid University_Roll No.');
-    field.set_value(0);
+        else if (data.friend_2s_crn.toString().length > 7 || data.friend_2s_crn.toString().length < 7) {
+            frappe.throw('Please Enter a valid College_Roll NO.');
+            return false;
 
-}
+        }
+        return true
+    }
 
-});
+    return true
 
-frappe.web_form.on('friend_2s_crn',(field,value)=>{
-   if (value.toString().length > 7 || value.toString().length < 7 ) {
-    frappe.throw('Please Enter a valid College_Roll NO.');
-    field.set_value(0);
-
-}
-
-});
-frappe.web_form.on('friend_2s_crn',(field,value)=>{
-   if (value.toString().length > 7 || value.toString().length < 7 ) {
-    frappe.throw('Please Enter a valid College_Roll NO.');
-    field.set_value(0);
-
-}
-
-});
+    }
 
 
-//  Name Validationz
-
-frappe.web_form.on('name1',(field,value)=>{
-   if (!isNaN(value) || value == " ") {
-    frappe.throw('Please Enter the valid name');
-    field.set_value(0);
-
-}
-
-});
-
-
-
-
-  }
 })
+
+
+
+
+
+
+
+
